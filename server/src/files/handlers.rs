3ce82e18ddf2e97998ba8e3aad81821b3dd1c1fa
NilsVreman <nils.vreman@gmail.com>
2024-10-16 21:07:07 +0200

    Refactor handler slightly to manage FileName structs instead (easier to sanitize)

--- server/src/files/handlers.rs
+++ server/src/files/handlers.rs
@@ -1,4 +1,7 @@
-use super::{errors::FileUploadError, storage};
+use super::{
+    errors::FileUploadError,
+    storage::{self, Sanitize},
+};
 
 use axum::{
     body,
@@ -8,21 +11,6 @@ use axum::{
 use serde::Serialize;
 
 pub const MAX_UPLOAD_SIZE: usize = 50 * 1024 * 1024; // 50 MB
-
-pub trait Sanitize {
-    fn sanitize(&self) -> Option<String>;
-}
-
-impl Sanitize for Option<&str> {
-    fn sanitize(&self) -> Option<String> {
-        self.map(|s| s.replace(&['/', '\\', ':', '*', '?', '"', '<', '>', '|'][..], ""))
-    }
-}
-impl Sanitize for String {
-    fn sanitize(&self) -> Option<String> {
-        Some(&self[..]).sanitize()
-    }
-}
 
 #[derive(Serialize)]
 pub struct ImageMetaData {
@@ -64,14 +52,10 @@ pub async fn post_image_list(
             .await
             .map_err(|err| FileUploadError::WriteFileError(err.to_string()))?;
 
-        let x = sanitized_name.clone();
-
         tokio::spawn(async move {
             storage::write_thumbnail(&sanitized_name, &image_data)
                 .map_err(|err| FileUploadError::WriteFileError(err.to_string()))
         });
-
-        dbg!("Uploaded file: {}", x);
     }
 
     Ok(Json(serde_json::json!(
@@ -103,7 +87,7 @@ pub async fn get_thumbnail(
 
 pub async fn get_all_thumbnail_meta_data() -> Result<Json<ImageList>, FileUploadError> {
     Ok(Json(ImageList {
-        images: storage::get_all_thumbnail_names()
+        images: storage::get_all_image_names()
             .await
             .map_err(|err| FileUploadError::ReadFileError(err.to_string()))?
             .iter()
--- server/src/files/storage.rs
+++ server/src/files/storage.rs
@@ -2,8 +2,12 @@ use axum::body;
 use core::fmt;
 use image::ImageError;
 use std::{io, path::PathBuf};
+use uuid::Uuid;
 
 const MAX_THUMBNAILS_SIZE: u32 = 100;
+static BAD_CHARS: &[char] = &[
+    '.', '/', '\\', ' ', '!', '<', '>', '|', ':', '(', ')', '&', ';', '#', '?', '*',
+];
 
 enum ImageType {
     Image,
@@ -19,6 +23,41 @@ impl fmt::Display for ImageType {
     }
 }
 
+pub struct FileName {
+    sanitized_name: String,
+    extension: String,
+    id: Uuid,
+}
+
+impl fmt::Display for FileName {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}_{}.{}", self.sanitized_name, self.id, self.extension)
+    }
+}
+
+pub trait Sanitize {
+    fn sanitize(&self) -> Option<FileName>;
+}
+
+impl Sanitize for Option<&str> {
+    fn sanitize(&self) -> Option<FileName> {
+        let bad_char = |c| BAD_CHARS.contains(&c) || c.is_control();
+        // get the first non-empty substring that doesn't contain bad characters
+        let file_name = self.and_then(|n| n.split(bad_char).find(|s| !s.is_empty()))?;
+        let file_extension = self.and_then(|n| n.rsplit_once('.')).map(|(_, ext)| ext)?;
+
+        if file_name.is_empty() {
+            return None;
+        }
+
+        Some(FileName {
+            sanitized_name: file_name.to_string(),
+            extension: file_extension.to_string(),
+            id: Uuid::new_v4(),
+        })
+    }
+}
+
 pub async fn setup() -> io::Result<()> {
     let image_folder = get_folder(ImageType::Image)?;
     let thumbnail_folder = get_folder(ImageType::Thumbnail)?;
@@ -27,20 +66,20 @@ pub async fn setup() -> io::Result<()> {
     ensure_folder_exists(&thumbnail_folder).await
 }
 
-pub async fn write_image(file_name: &String, image_data: &body::Bytes) -> Result<(), ImageError> {
-    tokio::fs::write(get_folder(ImageType::Image)?.join(&file_name), &image_data)
+pub async fn write_image(file_name: &FileName, image_data: &body::Bytes) -> Result<(), ImageError> {
+    tokio::fs::write(get_file_path(&file_name, ImageType::Image)?, &image_data)
         .await
         .map_err(|err| ImageError::IoError(err))
 }
 
-pub fn write_thumbnail(file_name: &String, image_data: &body::Bytes) -> Result<(), ImageError> {
+pub fn write_thumbnail(file_name: &FileName, image_data: &body::Bytes) -> Result<(), ImageError> {
     image::load_from_memory(&image_data)?
         .thumbnail(MAX_THUMBNAILS_SIZE, MAX_THUMBNAILS_SIZE)
-        .save(get_folder(ImageType::Thumbnail)?.join(&file_name))
+        .save(get_file_path(&file_name, ImageType::Thumbnail)?)
 }
 
-pub async fn get_all_thumbnail_names() -> io::Result<Vec<String>> {
-    let mut entries = tokio::fs::read_dir(get_folder(ImageType::Thumbnail)?).await?;
+pub async fn get_all_image_names() -> io::Result<Vec<String>> {
+    let mut entries = tokio::fs::read_dir(get_folder(ImageType::Image)?).await?;
     let mut image_names: Vec<String> = Vec::new();
     while let Some(entry) = entries.next_entry().await? {
         image_names.push(entry.file_name().to_string_lossy().to_string());
@@ -64,6 +103,10 @@ async fn get_image_type(name: &String, image_type: ImageType) -> io::Result<body
 
 fn get_folder(image_type: ImageType) -> io::Result<PathBuf> {
     std::env::current_dir().map(|x| x.join(image_type.to_string()))
+}
+
+fn get_file_path(file_name: &FileName, image_type: ImageType) -> io::Result<PathBuf> {
+    get_folder(image_type).map(|x| x.join(file_name.to_string()))
 }
 
 async fn ensure_folder_exists(folder: &PathBuf) -> io::Result<()> {
