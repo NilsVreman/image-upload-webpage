787c3801def3d721289817c8fc5a48c976133c3c
NilsVreman <nils.vreman@gmail.com>
2024-11-08 23:32:08 +0100

    Made server work with tls handshake, need to improve this slightly

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,39 @@
+use std::{
+    net::{Ipv4Addr, SocketAddr, TcpListener},
+    sync::Arc,
+};
+
+use axum_server::tls_rustls::RustlsConfig;
 use server;
 
+fn main() {
+    if let Err(e) = run_server() {
+        eprintln!("FAILED: {}", e);
+        std::process::exit(1);
+    }
+}
+
 #[tokio::main]
-async fn main() {
+async fn run_server() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    println!("Setting up server...");
+
     // Build our application
-    server::setup_app().await.unwrap();
-    let app = server::create_app();
+    let app = server::create_app().await?;
+
+    // Config auth
+    let tls_config = server::load_tls_config()?;
+    let axum_config = RustlsConfig::from_config(Arc::new(tls_config));
+
+    let port = 3000;
+    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), port);
 
     // Run our app
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    println!("Starting to serve on https://{}...", addr);
+
+    let listener = TcpListener::bind(addr)?;
+    axum_server::from_tcp_rustls(listener, axum_config)
+        .serve(app.into_make_service())
+        .await?;
+
+    Ok(())
 }
