50830fcd6cf44372c24c84e21845662be6911466
NilsVreman <nils.vreman@gmail.com>
2025-04-14 20:55:52 +0200

    feat(timeout): add request timeout and correct error handling

--- server/src/app.rs
+++ server/src/app.rs
@@ -5,19 +5,22 @@ use super::middleware;
 use axum::error_handling::HandleErrorLayer;
 use axum::middleware::from_fn_with_state;
 use axum::routing::get;
-use axum::{BoxError, Extension, Json, Router};
-use hyper::StatusCode;
+use axum::{Extension, Json, Router};
 use tower::buffer::BufferLayer;
 use tower::ServiceBuilder;
 use tower_http::services::{ServeDir, ServeFile};
 
 pub async fn create_app() -> Result<Router, String> {
+    // ### Setup ### //
     files::setup().await.map_err(|err| err.to_string())?;
 
+    // ### Configs ### //
     let jwt_config = auth::JwtConfig::from_env();
     let psw_config = auth::PasswordConfig::from_env();
 
+    // ### Routers ### //
     let public_routes = Router::new().route("/health", get(health_handler));
+
     let authenticated_routes =
         Router::new()
             .merge(files::create_image_router())
@@ -25,6 +28,7 @@ pub async fn create_app() -> Result<Router, String> {
                 jwt_config.clone(),
                 middleware::auth_middleware,
             ));
+
     let authorisation_routes = auth::create_authorisation_router()
         .layer(Extension(psw_config.clone()))
         .layer(Extension(jwt_config.clone()));
@@ -33,6 +37,7 @@ pub async fn create_app() -> Result<Router, String> {
         .merge(public_routes)
         .merge(authenticated_routes);
 
+    // ### Services and general middleware layers ### //
     // Create a service to serve static files from the `assets` folder.
     let serve_dir =
         ServeDir::new("assets").not_found_service(ServeFile::new("assets/not_found.html"));
@@ -40,20 +45,15 @@ pub async fn create_app() -> Result<Router, String> {
     // With fallback_service we serve assets directly from the root
     // 1. Add a Content-Security-Policy header to all responses.
     // 2. Add a rate limiter to all requests.
-    Ok(api_routes
-        .fallback_service(serve_dir)
-        .layer(middleware::content_security_policy_layer())
-        .layer(
-            ServiceBuilder::new()
-                .layer(HandleErrorLayer::new(|err: BoxError| async move {
-                    (
-                        StatusCode::INTERNAL_SERVER_ERROR,
-                        format!("Unhandled error: {}", err),
-                    )
-                }))
-                .layer(BufferLayer::new(1024))
-                .layer(middleware::rate_limiting_layer()),
-        ))
+    // 3. Add a timeout to all requests.
+    Ok(api_routes.fallback_service(serve_dir).layer(
+        ServiceBuilder::new()
+            .layer(HandleErrorLayer::new(middleware::handle_error)) // Handle later layer errors
+            .layer(BufferLayer::new(1024))
+            .layer(middleware::content_security_policy_layer()) // <- 1
+            .layer(middleware::rate_limiting_layer()) // <- 2
+            .layer(middleware::request_timeout_layer()), // <- 3
+    ))
 }
 
 async fn health_handler() -> Json<serde_json::Value> {
